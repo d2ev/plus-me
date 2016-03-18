db857723f672eecbc17942b6521dbe78b60c6189
phanimohan <y.phanimohan@gmail.com>
2016-03-18 15:33:15 +0000

    Update shell

--- plusme-update.php
+++ plusme-update.php
@@ -1,4 +1,5 @@
 <?php
-echo shell_exec('cd /var/www/html/plus-me/ && git pull');
+echo shell_exec('cd /var/www/html/plus-me/ && git pull https://phanimohan:plusme.care@github.com/d2ev/plus-me');
+//echo shell_exec('sh plusme-update.sh');
 die('Done. ' . mktime());
 ?>
