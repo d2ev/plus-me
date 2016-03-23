c79cfac133dbedffa5db1cb911598bd973c1f761
PHANi MOHAN <y.phanimohan@gmail.com>
2016-03-23 18:24:11 +0530

    Accessing user location

--- sites/all/themes/tweme/tweme.js
+++ sites/all/themes/tweme/tweme.js
@@ -1,5 +1,28 @@
 (function($) {
   $(function () {
-    // ...
+    // Code to get user location.
+    $.cookie.json = true;
+    if ($.cookie("user_location") !== '') {
+      console.log($.cookie("user_location"));
+    }
+    else {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          // console.log(position.coords);
+          var requestUrl = "http://ip-api.com/json";
+          $.ajax({
+            url: requestUrl,
+            type: 'GET',
+            success: function(json){
+              // console.log(json);
+              $.cookie("user_location", json);
+            },
+            error: function(err){
+              alert("Request failed, error= " + err);
+            }
+          });
+        });
+      }
+    }
   });
 })(jQuery);
